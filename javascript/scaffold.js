cd6e4f039c5f10e3ddaa6c5af0292518af26ed0e
Tim Felgentreff <timfelgentreff@gmail.com>
2014-11-15 16:05:40 +0100

    call test methods on 'this' (the test object)

--- javascript/scaffold.js
+++ javascript/scaffold.js
@@ -4,50 +4,44 @@ requires('lively.TestFramework',
          'users.timfelgentreff.z3.CommandLineZ3').
 toRun(function() {
 
-    function assert(v) { if (!v) { throw 'Assertion failed' } }
+function double() {
+    return 2 * this;
+}
+Number.prototype.double = double;
 
-    function fieldEquals(o1, o2) {
+TestCase.subclass('users.timfelgentreff.babelsberg.testsuite.SemanticsTests', {
+    fieldEquals: function(o1, o2) {
         for (var key in o1) {
-            if (o1[key] !== o2[key]) return false;
+            if (key[0] !== "$" && key[0] !== "_") {
+                if (o1[key] !== o2[key]) return false;
+            }
         }
         for (var key in o2) {
-            if (o1[key] !== o2[key]) return false;
+            if (key[0] !== "$" && key[0] !== "_") {
+                if (o1[key] !== o2[key]) return false;
+            }
         }
         return true;
-    }
-
-    function one() {
+    },
+    one: function() {
         return 1.0
-    }
-
-    function double() {
-        return 2 * this;
-    }
-    Number.prototype.double = double;
-
-    function Require_min_balance(acct, min) {
+    },
+    Require_min_balance: function(acct, min) {
         always: { acct.balance > min }
-    }
-
-    function Has_min_balance(acct, min) {
+    },
+    Has_min_balance: function(acct, min) {
         return acct.balance > min;
-    }
-
-    function Point(x, y) {
+    },
+    Point: function(x, y) {
         return {x: x, y: y}
-    }
-
+    },
     // TODO: others
-
-    function Test(i) {
+    Test: function(i) {
         ctx = {i: i};
         always: { priority: 'medium'; ctx.i == 5 }
         return ctx.i + 1
-    }
+    },
 
-
-
-TestCase.subclass('users.timfelgentreff.babelsberg.testsuite.SemanticsTests', {
 
 INSERTHERE
 
