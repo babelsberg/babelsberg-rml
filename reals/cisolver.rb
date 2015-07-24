52373977ce063888cbf4bcfcfb85f8e568051433
Tim Felgentreff <timfelgentreff@gmail.com>
2015-07-24 09:26:41 +0200

    update reals semantics to follow new paper semantics

--- reals/cisolver.rb
+++ reals/cisolver.rb
@@ -7,6 +7,7 @@ tree = Hash[*`ps -eo pid,ppid`.scan(/\d+/).map{|x|x.to_i}]
 shid = Process.ppid
 bbbid = tree[shid]
 catid = tree[bbbid]
+rakeid = tree[catid]
 commandline = `ps -o cmd -fp #{catid}`.lines.to_a.last
 example = /cat\s+(.*\d+\.txt)\s+/.match(commandline)[1]
 solution = example.sub(/\.txt$/, ".env")
